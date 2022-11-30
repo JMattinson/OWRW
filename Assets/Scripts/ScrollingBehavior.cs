b10280dd8537e319fda60bd5d706539d2c2da753
Jaden <jadenmattinson6@gmail.com>
2022-11-30 15:38:15 -0700

    Prep for Release
    -Added multiple obstacle variants
    -Fixed animation for coin lure trap
    -updated title screen text

--- Assets/Scripts/ScrollingBehavior.cs
+++ Assets/Scripts/ScrollingBehavior.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 public class ScrollingBehavior : MonoBehaviour
 {
 
-    [Range (-1f,1f)]
+    [Range(-1f, 1f)]
     public float speed;
-    
+
     private float offset;
     private Material mat;
     public BoolData stopCheck;
@@ -12,15 +12,15 @@ public class ScrollingBehavior : MonoBehaviour
     private void Start()
     {
         mat = GetComponent<Renderer>().material;
-        
+
     }
-    
-    
+
+
     public void BGScroll()
     {
         if (stopCheck)
         {
-            offset += (speed* Time.deltaTime);
+            offset += (speed * Time.deltaTime);
             mat.SetTextureOffset("_MainTex", new Vector2(offset, 0));
         }
 
