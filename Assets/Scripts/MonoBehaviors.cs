95a175ff4784fefa371cc56aaa9a267d7fbade24
Jaden <jadenmattinson6@gmail.com>
2022-12-03 15:12:03 -0700

    Minor bugfixing
    added a temporary fix to score saving, will clean up Monday.

--- Assets/Scripts/MonoBehaviors.cs
+++ Assets/Scripts/MonoBehaviors.cs
@@ -1,9 +1,6 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-using System.Collections;
-using UnityEngine;
 using UnityEngine.Events;
 
 public class MonoBehaviors : MonoBehaviour
